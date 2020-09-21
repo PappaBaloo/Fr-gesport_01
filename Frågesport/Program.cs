ad7d3214f7d6f8a4b69a28a1893a299ba7fc03b5
Charlie Sesser Nylund <charlie.sessernylun@learnet.se>
2020-09-21 08:57:22 +0200

    first commit

---
+++ Frågesport/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Frågesport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int points = 0;
+
+            if (points == 0)
+            {
+                points = 1;
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
